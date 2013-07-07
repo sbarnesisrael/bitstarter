93418600b32edf776c53b9febe3982cab2b19830
Spencer Barnes-Israel <s.barnes.israel@gmail.com>
2013-07-07 20:22:07 +0000

    Still trying

--- web.js
+++ web.js
@@ -6,10 +6,8 @@ app.get('/', function(request, response) {
 //  response.send('Hello World2!');
     var fs = require('fs');
     var file = './index.html';
-    var buffer = new Buffer(512);
-    var contents = fs.readFileSync(file);
-    var buff = buffer.write(contents);
-    buf.toString("utf-8");
+    var buff = new Buffer(fs.readFileSync(file));
+    response.send(buff.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
