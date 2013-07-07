71d9eac842d12626c28711fdd470f5599d304be5
Spencer Barnes-Israel <s.barnes.israel@gmail.com>
2013-07-07 20:15:06 +0000

    Still trying to figure it out.

--- web.js
+++ web.js
@@ -7,7 +7,8 @@ app.get('/', function(request, response) {
     var fs = require('fs');
     var file = './index.html';
     var buffer = new Buffer(512);
-    var buff = buffer.write(fs.readFileSync(file));
+    var contents = fs.readFileSync(file);
+    var buff = buffer.write(contents);
     buf.toString("utf-8");
 });
 
