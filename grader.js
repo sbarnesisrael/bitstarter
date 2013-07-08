0e0642576b43708800c1aeeadd8143a93339f056
Spencer Barnes-Israel <s.barnes.israel@gmail.com>
2013-07-08 00:42:19 +0000

    Shots in the dark - adding --url option

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,12 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var restlerURL = function(url) {
+    restler.get(url).on('complete', function(result) {
+	return result;
+    }
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,9 +73,10 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url address>', 'URL to index.html', clone(restlerURL), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
+    var outJson = JSON.stringifycheckJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
